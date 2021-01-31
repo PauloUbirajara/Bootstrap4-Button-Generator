dab73fb51e1417b018497ba46809d543679b1b87
PauloUbirajara <ubirajara3@hotmail.com>
2021-01-31 01:24:59 -0300

    Preparando página para modificação de componente

--- index.js
+++
@@ -1,8 +0,0 @@
-import ElementController from './controller/ElementController';
-import Button from './model/Button';
-
-//@ Testar o backend antes de ir pro React
-
-const ec = new ElementController(new Button());
-ec.setColor('-primary');
-console.log(ec.build());
--- model/Button.ts
+++
@@ -1,8 +0,0 @@
-import DefaultElement from "./DefaultElement";
-
-export default class Button extends DefaultElement {
-    constructor() {
-        super();
-        this.mainElement = 'button';
-    }
-}
